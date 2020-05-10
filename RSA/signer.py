81e6a087426c7268c863a7c51fc4e83a9135f028
Dhriti Garg <58422781+dhritigarg02@users.noreply.github.com>
2020-05-10 13:51:29 +0530

    Update signer.py

--- RSA/signer.py
+++ RSA/signer.py
@@ -8,12 +8,12 @@ private_key = input('Enter private key: ')
 message = input('Enter message: ')
 
 try:
-    message = open(message).read()
+    message = open(message, 'rb').read()
 except:
     pass
 
 pkey = RSA.import_key(open(private_key).read())
-h = SHA256.new(message.encode())
+h = SHA256.new(message)
 signature =  pss.new(pkey).sign(h)
 
 f = open('signature.txt', 'wb')
