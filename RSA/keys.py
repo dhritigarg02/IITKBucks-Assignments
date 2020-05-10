bd3a3f37c13aac8e2ca7961f1c9d5862226f324d
Dhriti Garg <58422781+dhritigarg02@users.noreply.github.com>
2020-05-10 13:35:50 +0530

    Add files via upload

---
+++ RSA/keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from Crypto.PublicKey import RSA
+
+key_pair = RSA.generate(2048)
+
+private_key = key_pair.exportKey("PEM")
+public_key = key_pair.publickey().exportKey("PEM")
+
+sk = open("privatekey2.pem", "wb")
+sk.write(private_key)
+sk.close()
+
+pk = open("publickey2.pem", "wb")
+pk.write(public_key)
+pk.close()
