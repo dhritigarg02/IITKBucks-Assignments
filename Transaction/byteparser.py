8bf3d4e50f67df7a9d5f48fec477c8973dfcad52
Dhriti Garg <58422781+dhritigarg02@users.noreply.github.com>
2020-05-14 16:14:33 +0530

    Update and rename parser.py to byteparser.py

--- Transaction/byteparser.py
+++ Transaction/byteparser.py
@@ -17,13 +17,17 @@ curr_index = 4
 for i in range(num_inputs):
 
     print('\tInput ' + str(i+1) + ':')
+    
     print('\t\tTransaction ID: ', fh[curr_index:curr_index + 32].hex())
     curr_index += 32
+    
     print('\t\tIndex: ', int.from_bytes(fh[curr_index:curr_index + 4], byteorder = 'big'))
     curr_index += 4
+    
     len_sig = int.from_bytes(fh[curr_index:curr_index + 4], byteorder = 'big')
     print('\t\tLength of the signature: ', len_sig)
     curr_index += 4
+    
     print('\t\tSignature: ', fh[curr_index:curr_index + len_sig].hex(), '\n')
     curr_index += len_sig 
 
@@ -35,11 +39,14 @@ curr_index += 4
 for i in range(num_outputs):
     
     print('\tOutput ' + str(i+1) + ':')
+    
     print('\t\tNumber of coins: ', int.from_bytes(fh[curr_index:curr_index+8], 'big'))
     curr_index += 8
+    
     len_pubkey = int.from_bytes(fh[curr_index:curr_index+4], 'big')
     print('\t\tLength of public key: ', len_pubkey)
     curr_index += 4
+    
     wrapper = TextWrapper(subsequent_indent = '\t\t            ')
     pubkey = wrapper.wrap(text = fh[curr_index:curr_index + len_pubkey].decode('utf-8'))
     print('\t\tPublic key: ', end = '')
