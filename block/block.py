4250ace1a1b307c0f56bf4eafcac5b9873b2d0ae
Dhriti Garg <58422781+dhritigarg02@users.noreply.github.com>
2020-05-23 23:05:32 +0530

    Update block.py

--- block/block.py
+++ block/block.py
@@ -5,17 +5,17 @@ import time
 
 class block:
 
-    def __init__(self, index, prev_hash, target, data):
+    def __init__(self, index, parent_hash, target, data):
 
         self.index = index
-        self.prev_hash = bytes.fromhex(prev_hash)
+        self.parent_hash = bytes.fromhex(parent_hash)
         self.target = target
         self.data = open(data, 'rb').read()
         self.data_hash = hashlib.sha256(self.data).digest()
 
     def mine(self, max_nonce):
 
-        block_header_inits = index.to_bytes(4, 'big') + self.prev_hash + self.data_hash + target.to_bytes(32, 'big')
+        block_header_inits = index.to_bytes(4, 'big') + self.parent_hash + self.data_hash + target.to_bytes(32, 'big')
 
         h = hashlib.sha256()
         h.update(block_header_inits)
@@ -38,13 +38,13 @@ class block:
                 break
 
 index = int(input('Enter index of block: '))
-prev_hash = input('Enter hash of previous block: ')
+parent_hash = input('Enter hash of parent block: ')
 target = int(input('Enter target: '), 16)
 data = input('Enter path to block body file: ')
 
 max_nonce = 100000000000
 
-block1 = block(index, prev_hash, target, data)
+block1 = block(index, parent_hash, target, data)
 block1.mine(max_nonce)
 
 print('The found nonce value is: ', block1.nonce)
